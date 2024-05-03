68b79b2261a7b845f6e2e893fdb02454e58c97bc
Laurencia1806 <100884234+Laurencia1806@users.noreply.github.com>
2024-05-03 18:23:27 +0200

    Ajout repertoire java

---
+++ app/src/main/java/com/example/application_interim/InscriptionCandidatActivity.java
@@ -0,0 +1,14 @@
+package com.example.application_interim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InscriptionCandidatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription_candidat);
+    }
+}
