007b413a649e01c13ce292a9ea10e39f6f088752
Marta Boshkovska <22012535@etu.unicaen.fr>
2024-05-06 13:10:39 +0200

    Firebase setup

--- app/src/main/java/com/example/application_interim/MainActivity.java
+++ app/src/main/java/com/example/application_interim/MainActivity.java
@@ -4,13 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    DatabaseReference dataBaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        dataBaseReference = FirebaseDatabase.getInstance().getReference("this is the path");
+        dataBaseReference.setValue("Hello, World!").addOnSuccessListener(this, aVoid -> {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(this, e -> {
+            Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show();
+        }).addOnCompleteListener(this, task -> {
+            Toast.makeText(this, "Complete", Toast.LENGTH_SHORT).show();
+        });
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
